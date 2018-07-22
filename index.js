a881ae9ebde3bc0beae95463b0f108b91dcaf210
Ann Siebecker <asiebecker@hotmail.com>
2018-07-22 23:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  if (someValue <= 400) {
+    result = "This one is on me!";
+    return result;
+  }
+    else if (someValue > 2000 && someValue <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+    return result;
+  }
+    else if (someValue > 2500) {
+    result = "No can do.";
+    return result;
+  }
+}
+
+function ternaryCheckCity(cityname) {
+    let result
+    result = (cityname==='NYC') ? "Ok, sounds good.":"No go.";
+    return result;
+  }
