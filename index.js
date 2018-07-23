311c03a28c9efde0a029392ac094ff163d54a130
Ann Siebecker <asiebecker@hotmail.com>
2018-07-23 00:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function ternaryCheckCity(cityname) {
   
   function switchOnCharmFromTip(tiptype) {
     let result
-    result = (cityname==='NYC') ? "Ok, sounds good.":"No go.";
+    {
     return result;
   }
