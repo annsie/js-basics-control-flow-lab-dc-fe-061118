d5b57d8bd0be6f55fdc2bef7c8e6def7a26e986f
Ann Siebecker <asiebecker@hotmail.com>
2018-07-23 00:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function ternaryCheckCity(cityname) {
       case 'not as generous':
         result = "Thank you.";
         break;
-      
+      default:
+        result = ""
     }
     return result;
   }
